3ae65682704e386ae6a0b3025a7c300993814a9b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-06-11 22:47:48 -0400

    adding concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,10 +12,27 @@ module.exports = function(grunt) {
           'drop.js': ['drop.coffee'] // compile and concat into single file
         }
       }
+    },
+
+    concat: {
+      options: {
+        separator: ';\n'
+      },
+      dist: {
+        src: [
+          'components/angular/angular.min.js',
+          'components/bespoke.js/dist/bespoke.min.js',
+          'components/markdown/lib/markdown.js',
+          'components/jquery/jquery.min.js',
+          'drop.js'
+        ],
+        dest: 'dist/slides-now.js'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-bump');
 
   grunt.registerTask('default', ['coffee']);
