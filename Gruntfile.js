05d1af453cc3bb5753776cf1baf0b5fc46bad969
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-06-11 23:06:32 -0400

    added all tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,13 +47,31 @@ module.exports = function(grunt) {
         ],
         dest: 'dist/slides-now.min.js'
       }
+    },
+
+    copy: {
+      main: {
+        files: {
+          'dist/index.html': 'index.html',
+          'dist/README.md': 'README.md',
+          'dist/drop.css': 'css/drop.css',
+          'dist/style.css': 'components/bespoke.js/demo/style.css',
+          'dist/themes.css': 'components/bespoke.js/demo/themes.css'
+          /*
+          'dist/slides-now.css': ['css/drop.css',
+            'components/bespoke.js/demo/style.css',
+            'components/bespoke.js/demo/themes.css']
+          */
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-bump');
 
-  grunt.registerTask('default', ['coffee', 'uglify', 'concat:dev']);
+  grunt.registerTask('default', ['coffee', 'uglify', 'concat:dev', 'copy']);
 };
